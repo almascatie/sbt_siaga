72a54c8d611955ac104734bc6236c802cdaf7532
almascatie <31232574+almascatie@users.noreply.github.com>
2026-08-03 09:28:01 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+const CONFIG = {
+    MAPBOX_TOKEN: 'pk.eyJ1IjoiYWxtYXNjYXRpZSIsImEiOiJjbHE3YzVhMGgwdzVjMmlwczVlaWhpMmJ2In0.C98KArIa5on5kkr9C3Plvg',
+    SUPABASE_URL: 'https://tpopgxvuwdjqaaltssuf.supabase.co',
+    SUPABASE_ANON_KEY: 'sb_publishable_qfy-1Xfaa3MV5HqvEmOQ_tzT6qEPm'
+};
