42f4821fd3d35ce5d6425269a1840f95b272f9cd
almascatie <31232574+almascatie@users.noreply.github.com>
2026-08-03 10:07:02 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const CONFIG = {
     MAPBOX_TOKEN: 'pk.eyJ1IjoiYWxtYXNjYXRpZSIsImEiOiJjbHE3YzVhMGgwdzVjMmlwczVlaWhpMmJ2In0.C98KArIa5on5kkr9C3Plvg',
     SUPABASE_URL: 'https://tpopgxvuwdjqaaltssuf.supabase.co',
-    SUPABASE_ANON_KEY: 'sb_publishable_qfy-1Xfaa3MVa5HqvEEmOQ_tzT6qEPm'
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwb3BneHZ1d2RqcWFhbHRzc3VmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU2NTM0NjEsImV4cCI6MjEwMTIyOTQ2MX0.RwSzeGbnxrGCzY4KIDrbyHU9gnUyW7EqwSxaRsHO4rY'
 };
 
 // Inisialisasi Supabase khusus untuk format Publishable Key baru
