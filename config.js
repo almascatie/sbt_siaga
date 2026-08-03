cda7bdca045e1f9bc5e940a6669ebf29c5f609da
almascatie <31232574+almascatie@users.noreply.github.com>
2026-08-03 10:10:41 +0900

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,4 @@ const CONFIG = {
     SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwb3BneHZ1d2RqcWFhbHRzc3VmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU2NTM0NjEsImV4cCI6MjEwMTIyOTQ2MX0.RwSzeGbnxrGCzY4KIDrbyHU9gnUyW7EqwSxaRsHO4rY'
 };
 
-// Inisialisasi Supabase khusus untuk format Publishable Key baru
-const supabaseClient = supabase.createClient(CONFIG.SUPABASE_URL, CONFIG.SUPABASE_ANON_KEY, {
-    auth: { persistSession: false }
-});
+
